f003510eaa8348db7a9de2b9b1e82898257acce0
mrkantarci <feritkantarci@gmail.com>
2024-02-04 23:36:56 +0300

    bolum6odevleri

---
+++ JavaEgitim/Calismalarim/Section06Homeworks/src/s06q01.java
@@ -0,0 +1,8 @@
+public class s06q01 {
+    //Write a program to print numbers from 1 to 10.
+    public static void main(String[] args) {
+        for(int i=1;i<=10;i++){
+            System.out.println(i);
+        }
+    }
+    }
